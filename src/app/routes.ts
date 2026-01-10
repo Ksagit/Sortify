7c56199d13862357b0b594435e4d09847a5e3993
ksagit <kuba.kielaszek@gmail.com>
2026-01-10 17:11:55 +0100

    Complete repository restructure

--- src/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes"
-
-export default [
-  index("routes/landing-page.tsx"),
-  route("Dashboard", "routes/dashboard.tsx"),
-] satisfies RouteConfig
